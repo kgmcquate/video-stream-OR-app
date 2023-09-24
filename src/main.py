cce0d736025c3b916801f86bfebe1cf2af9cb032
kevin <you@example.com>
2023-09-24 14:39:51 -0500

    person detection on yt live stream working

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from typing import Callable, Any
+
 # from pytube import YouTube
 from urllib.parse import urlunsplit, urlencode
 from pydantic.dataclasses import dataclass
+import dataclasses
 
 # import libraries
 from vidgear.gears import CamGear
@@ -13,6 +16,36 @@ class Config:
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 person_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
+
+class BaseObjectDetector:
+    detector_name: str = None
+    target_object_name: str = None
+
+    def process(self, image: np.array) -> (np.array, dict[str, Any]):
+        """Returns processed image and metadata dict"""
+        pass
+
+@dataclass
+class PersonHAARClassifier(BaseObjectDetector):
+    detector_name = "haar_classifier"
+    target_object_name = "person"
+
+    def process(self, image: np.array) -> (np.array, dict[str, Any]):
+        """Returns processed image and metadata dict"""
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Getting corners around the face
+        # 1.3 = scale factor, 5 = minimum neighbor can be detected
+        objs = person_classifier.detectMultiScale(img_gray, 1.05, 3)  
+
+        # drawing bounding box around face
+        for (x, y, w, h) in objs:
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255,   0), 3)
+
+        # displaying image with bounding box
+        # cv2.imshow('face_detect', img)
+        return image, {"num_objects": len(objs), "object_bboxes": objs}
+
 @dataclass(config=Config)
 class VideoStream:
     streaming_service: str
@@ -20,6 +53,7 @@ class VideoStream:
     capture_fps: float
     url: str = None
     stream: CamGear = None
+    object_detectors: list[BaseObjectDetector]
 
     def __post_init__(self):
         base_url = "http://youtube.com"
@@ -42,19 +76,7 @@ class VideoStream:
     @staticmethod
     def process_frame(frame) -> np.array:
 
-        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Getting corners around the face
-        # 1.3 = scale factor, 5 = minimum neighbor can be detected
-        faces = person_classifier.detectMultiScale(img_gray, 1.05, 3)  
-
-        # drawing bounding box around face
-        for (x, y, w, h) in faces:
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255,   0), 3)
-
-        # displaying image with bounding box
-        # cv2.imshow('face_detect', img)
-        return frame
+        pass
 
     def start_stream(self):
         self.stream.start()
