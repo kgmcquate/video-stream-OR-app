dbe5de8a0004318a2c9e528fdb3f8640311d8246
kevin <you@example.com>
2023-09-28 18:51:27 -0500

    using json for processed image

--- image-classifier/src/app/main.py
+++ image-classifier/src/app/main.py
@@ -56,49 +56,50 @@ import io
 
 from .classifiers import RawImageRecord
 
-spark = SparkSession.builder.getOrCreate()
+
 
 def bytes_with_schema_to_avro(binary, avro_read_schema=parse_schema(avro_schema)):
     with io.BytesIO(binary) as bytes_io:
         reader = fastavro.reader(bytes_io, avro_read_schema)
         return next(reader)
 
+def main():
+    spark = SparkSession.builder.getOrCreate()
 
-
-stream = (
-    spark
-    .readStream
-    .format("kafka")
-    .options(
-        **{f"kafka.{k}": v for k, v in kafka_config.items()}
+    stream = (
+        spark
+        .readStream
+        .format("kafka")
+        .options(
+            **{f"kafka.{k}": v for k, v in kafka_config.items()}
+        )
+        .option("subscribe", raw_video_frames_topic_name)
+        .load()
+        .select("key", "value")
+        .rdd
+        .mapValues(bytes_with_schema_to_avro)
+        .map(lambda key, record: RawImageRecord(key=key, **record))
+        .flatMapValues(lambda record: record.process_image())
+        .map(lambda key, image: {
+                "key": f"{key}_{image.detector_name}_{image.object_name}",
+                "value": image.to_json()
+            }
+        )
     )
-    .option("subscribe", raw_video_frames_topic_name)
-    .load()
-    .select("key", "value")
-    .rdd
-    .mapValues(bytes_with_schema_to_avro)
-    .map(lambda key, record: RawImageRecord(key=key, **record))
-    .flatMapValues(lambda record: record.process_image())
-    .map(lambda key, image: {
-            "key": f"{key}_{image.detector_name}_{image.object_name}",
-            "value": image.to_json()
-        }
+
+    write_schema = StructType([
+        StructField("key", StringType()),
+        StructField("value", StringType())
+    ])
+
+    (
+        spark.createDataFrame(stream, schema=write_schema)
+        .writeStream
+        .format('kafka')
+        .options(
+            **{f"kafka.{k}": v for k, v in kafka_config.items()}
+        )
+        .option("topic", processed_video_frames_topic_name)
+        .save()
+
     )
-)
-
-write_schema = StructType([
-    StructField("key", StringType()),
-    StructField("value", StringType())
-])
-
-(
-    spark.createDataFrame(stream, schema=write_schema)
-    .writeStream
-    .format('kafka')
-    .options(
-        **{f"kafka.{k}": v for k, v in kafka_config.items()}
-    )
-    .option("topic", processed_video_frames_topic_name)
-    .save()
-
-)
