546f89d3f5f088af85aa5467065add1243afc40d
kevinmcquate <kgmcquate@gmail.com>
2024-08-28 18:40:15 -0400

    fix: fixed youtube creds

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,9 @@ import json
 import os
 import boto3
 
-secret_arn = os.environ.get("DB_CREDS_SECRET_ARN", "arn:aws:secretsmanager:us-east-1:117819748843:secret:lake-freeze-db-creds")
+secret_arn = os.environ.get("DB_CREDS_SECRET_ARN", "arn:aws:secretsmanager:us-east-1:117819748843:secret:main-rds-db-creds")
 
-db_endpoint = os.environ.get("DB_ENDPOINT" , "lake-freeze-db.cu0bcthnum69.us-east-1.rds.amazonaws.com")
+db_endpoint = os.environ.get("DB_ENDPOINT" , "main-db.cu0bcthnum69.us-east-1.rds.amazonaws.com")
 
 
 print("getting creds from sm")
